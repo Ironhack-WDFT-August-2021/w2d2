21496598448c67932ea3bf59919574d6f622bd7b
Jan Redmann <janredmann1@gmail.com>
2021-08-17 14:00:14 +0200

    add starting file

---
+++ oop/index.js
@@ -0,0 +1,21 @@
+// OOP - Object Oriented Programming - a programming paradigm
+
+const squares = [100, -10, 0, 0, -40, -10, -10, 5, 0, -10, -50, -10, 0, 0, -50, -10];
+
+let dice;
+
+const player1 = {
+	name: 'Bob',
+	color: 'red',
+	position: 0,
+	cash: 1000
+}
+
+const player2 = {
+	name: 'Alice',
+	color: 'blue',
+	position: 0,
+	cash: 1000
+}
+
+// 
